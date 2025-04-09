96388e37836f65b2a03388939c18408165d98a7b
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-04-09 12:28:58 +0545

    initital commit

---
+++ array1.java
@@ -0,0 +1,17 @@
+class Arraydemonstrtion{
+
+    public static void main(String[] args)
+    {
+         int[] num = {1,2,3};
+
+         // accessing the arrays
+
+         System.out.println("first array = "+num[0]);
+         System.out.println("second array = "+num[1]);
+         System.out.println("third array = "+num[2]);
+
+    }
+
+
+
+}
