2c68332c3d735b9c76dc37f10a0841055c0801c4
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-04-26 17:48:20 +0545

    this is user defined package

---
+++ AddOperation.java
@@ -0,0 +1,11 @@
+// here i am creating the user defined package 
+// previously i learnt about the built in package 
+// we can use buit in package using import java.util.Scanner for the read operation and for other operation we can use java.util.* this includes data, time and othe roperations
+package myfirstpackage;
+class AddOperation
+{
+    public int Add(int a, int b)
+    {
+        return (a+b);
+    }
+}
