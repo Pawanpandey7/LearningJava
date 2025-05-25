2335b734ea108120ab2282c17be9dbdfcadafecb
Pawanpandey7 <pawanpandey2058@gmail.com>
2025-05-25 13:57:35 +0545

    Menu and sub menu

---
+++ MenuExample.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import javax.swing.*;
+
+class MenuExample{
+    public static void main(String args[])
+    {
+        JFrame fr = new JFrame("menu and submenu");
+        JMenuBar mb = new JMenuBar();
+        JMenu menu = new JMenu("menu");
+        JMenu submenu = new JMenu("submenu");
+        JPanel panel = new JPanel();
+        JMenuItem i1 = new JMenuItem("1");
+        JMenuItem i2 = new JMenuItem("2");
+        JMenuItem i3 = new JMenuItem("3");
+        JMenuItem i4 = new JMenuItem("4");
+        JMenuItem i5 = new JMenuItem("5");
+        mb.add(menu);
+        panel.add(mb);
+        fr.add(panel);
+        menu.add(i1);
+        menu.add(i2);
+        submenu.add(i3);
+        submenu.add(i4);
+        submenu.add(i5);
+        menu.add(submenu);
+        fr.setVisible(true);
+        fr.setSize(500,500);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
